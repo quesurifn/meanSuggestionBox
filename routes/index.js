a5541898c9be1a6817c1f44ce38d7165b2919643
Kyle <kyle.c.r.fahey@gmail.com>
2016-12-13 23:31:49 -0600

    fix capitalization

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var router = express.Router();
 var jwt = require('express-jwt');
 var passport = require('passport');
 var mongoose = require('mongoose');
-var Post = mongoose.model('post');
-var Comment = mongoose.model('comment');
-var User = mongoose.model('user');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
